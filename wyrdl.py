808a68510d91ac0d39a5cc8d20570a0d26bf4c36
carvalhocaio <caiocarvalho.py@gmail.com>
2023-02-21 12:22:59 -0400

    step 3: organize the code with functions

--- wyrdl.py
+++ wyrdl.py
@@ -2,22 +2,51 @@ import pathlib
 import random
 from string import ascii_letters
 
-WORDLIST = pathlib.Path("wordlist.txt")
 
-words = [
-    word.upper()
-    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
-    if len(word) == 5 and all(letter in ascii_letters for letter in word)
-]
-word = random.choice(words)
+def main():
+    # Pre-process
+    word_path = pathlib.Path(__file__).parent / "wordlist.txt"
+    word = get_random_word(word_path.read_text(encoding="utf-8").split("\n"))
 
-for guess_num in range(1, 7):
-    guess = input(f"\nGuess {guess_num}: ").upper()
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
 
-    if guess == word:
-        print("Correct")
-        break
+        show_guess(guess, word)
+        if guess == word:
+            break
 
+    # Post-process
+    else:
+        game_over(word)
+
+
+def get_random_word(word_list):
+    """Get a random five-letter word from a list of strings.
+
+    ## Example:
+
+    >>> get_random_word(["snake", "worm", "it'll"])
+    'SNAKE'
+    """
+    words = [
+        word.upper()
+        for word in word_list
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
+    return random.choice(words)
+
+
+def show_guess(guess, word):
+    """Show the user's guess on the terminal and classify all letters.
+
+    ## Example:
+
+    >>> show_guess("CRANE", "SNAKE")
+    Correct letters: A, E
+    Misplaced letters: N
+    Wrong letters: C, R
+    """
     correct_letters = {
         letter for letter, correct in zip(guess, word) if letter == correct
     }
@@ -28,5 +57,10 @@ for guess_num in range(1, 7):
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
-else:
+
+def game_over(word):
     print(f"The word was {word}")
+
+
+if __name__ == "__main__":
+    main()
